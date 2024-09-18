0549fb31e72f73b93463db0faa96fc1d16fdb891
Christian Hohenbild <mail@christianhohenbild.de>
2024-09-18 18:14:10 +0200

    fix gradient scaling and add minimal size

--- src/App.jsx
+++ src/App.jsx
@@ -40,27 +40,34 @@ const InteractiveLink = React.memo(({ url, text, position }) => {
   )
 })
 
-const GradientBackground = React.memo(({ width, height, color1, color2 }) => {
-  const texture = useMemo(() => {
-    const canvas = document.createElement("canvas")
-    canvas.width = 256
-    canvas.height = 1
-    const ctx = canvas.getContext("2d")
-    const gradient = ctx.createLinearGradient(0, 0, 256, 0)
-    gradient.addColorStop(0, color1)
-    gradient.addColorStop(1, color2)
-    ctx.fillStyle = gradient
-    ctx.fillRect(0, 0, 256, 1)
-    return new THREE.CanvasTexture(canvas)
-  }, [color1, color2])
+const GradientBackground = React.memo(
+  ({ color1, color2, scaleX = 1, scaleY = 1, minWidth = 0, minHeight = 0 }) => {
+    const { viewport } = useThree()
 
-  return (
-    <mesh>
-      <planeGeometry args={[width, height]} position={[0, 0, -1]} />
-      <meshBasicMaterial map={texture} />
-    </mesh>
-  )
-})
+    const texture = useMemo(() => {
+      const canvas = document.createElement("canvas")
+      canvas.width = 256
+      canvas.height = 1
+      const ctx = canvas.getContext("2d")
+      const gradient = ctx.createLinearGradient(0, 0, 256, 0)
+      gradient.addColorStop(0, color1)
+      gradient.addColorStop(1, color2)
+      ctx.fillStyle = gradient
+      ctx.fillRect(0, 0, 256, 1)
+      return new THREE.CanvasTexture(canvas)
+    }, [color1, color2])
+
+    const width = Math.max(viewport.width * scaleX, minWidth)
+    const height = Math.max(viewport.height * scaleY, minHeight)
+
+    return (
+      <mesh>
+        <planeGeometry args={[width, height]} />
+        <meshBasicMaterial map={texture} />
+      </mesh>
+    )
+  }
+)
 
 function Buffer({ children }) {
   const planeRef = useRef()
@@ -113,16 +120,18 @@ export default function App() {
     <Canvas shadows camera={{ position: [0, 0, 4], fov: 40 }}>
       <Buffer>
         <GradientBackground
-          width={6}
-          height={10}
-          color1="black"
-          color2="white"
+          color1="#000000"
+          color2="#FFFFFF"
+          scaleX={1}
+          scaleY={1}
         />
         <GradientBackground
-          width={1.2}
-          height={1.4}
           color1="#FFD500"
           color2="#0000FF"
+          scaleX={0.16}
+          scaleY={0.28}
+          minWidth={0.8} // Minimum width in Three.js units
+          minHeight={1.4} // Minimum height in Three.js units
         />
         {links.map((link, index) => (
           <InteractiveLink key={index} {...link} position={[0, link.y, 0]} />
